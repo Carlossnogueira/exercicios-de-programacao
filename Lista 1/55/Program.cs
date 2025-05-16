f22834d267e9891ff9f7aa62f281f18879eb3ed8
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-05-16 19:52:32 -0300

    exercicio 55

---
+++ Lista 1/55/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace cinquentaecinco;
+
+/*
+    Fazer  um  programa  que  leia  uma  frase  de  até  50  caracteres  e  imprima  a  frase  sem  os  espaços  em  branco. 
+    Imprimir também a quantidade de espaços em branco da frase.
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        string frase = "";
+        string fraseSemEspaco = "";
+        int espaços = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Digite uma frase com até 50 caracteres: ");
+            frase = Console.ReadLine() ?? "null";
+
+            if (frase == "null")
+            {
+                Console.WriteLine("Entre com um valor válido!");
+                continue;
+            }
+
+            if (frase.Length > 50)
+            {
+                Console.WriteLine("Frase muito grande! Digite novamente:");
+                continue;
+            }
+
+            break;
+        }
+
+        for (int i = 0; i < frase.Length; i++)
+        {
+            if (frase[i] == ' ')
+            {
+                espaços++;
+            }
+
+            if (frase[i] != ' ') // queria que frase[i] fosse diferente de ' ' 
+            {
+                fraseSemEspaco += frase[i];
+            }
+        }
+
+        Console.WriteLine($"Frase sem espaços: {fraseSemEspaco}");
+        Console.WriteLine($"Quantidade de espaços: {espaços}");
+
+    }
+}
