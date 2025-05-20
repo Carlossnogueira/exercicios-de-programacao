2083d78017d80a23b2e1006adcf0d78aca640240
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-05-20 19:39:46 -0300

    exercicio 57

---
+++ Lista 1/57/57.js
@@ -0,0 +1,15 @@
+//  Fazer um programa que leia uma frase e imprima somente as vogais. 
+
+const frase = prompt("Digite uma frase sem acentos!").toLowerCase()
+const vogais = ["a","e","i","o","u"]
+let vogaisNaFrase = ""
+
+for(i= 0 ; i < frase.length; i++){
+    for(j= 0 ; j < vogais.length; j++){
+        if(frase[i] === vogais[j]){
+           vogaisNaFrase += frase[i]
+        }
+    }
+}
+
+console.log(vogaisNaFrase)
